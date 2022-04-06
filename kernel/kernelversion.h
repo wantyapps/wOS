2ee408631afff25925643db0a5b60f249dedb16e
Uri Arev <me@wantyapps.xyz>
2022-04-06 09:56:47 +0300

    Bump version
    Signed-off-by: Uri Arev <me@wantyapps.xyz>

--- kernel/kernelversion.h
+++ kernel/kernelversion.h
@@ -1,6 +1,6 @@
 int VERSION = 1;
 int PATCHLEVEL = 1;
 int SUBLEVEL = 0;
-char EXTRAVERSION[] = "-rc6";
+char EXTRAVERSION[] = "";
 char NAME[] = "wOS";
-char FULLVERSION[] = "1.1.0-rc6 (wOS)";
+char FULLVERSION[] = "1.1.0 (wOS)";
