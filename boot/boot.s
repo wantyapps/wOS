57f44e40b61f494c97afc41ed1ea1d92b5eea82c
Uri Arev <me@wantyapps.xyz>
2022-07-08 19:25:13 +0300

    Revert boot.s to a more "working" state

--- boot/boot.s
+++ boot/boot.s
@@ -9,44 +9,19 @@
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
-
+ 
 .section .bss
 .align 16
 stack_bottom:
 .skip 16384
 stack_top:
-
-BOOT_DRIVE:
  
 .section .text
 .global _start
 .type _start, @function
 _start:
-	
-	
 	mov $stack_top, %esp
  
-	mov %dh, 22
-	mov %dl, %dl
-
-	pusha
-
-	push %dx
-
-	mov %ah, 0x02
-	mov %al, %dh
-	mov %cl, 0x02 
-		 
-	mov %ch, 0x00 
-
-	mov %dh, 0x00 
-	int $0x13
-
-	pop %dx
-	cmp %al, %dh    
-
-	popa
-	ret
 	call kmain
  
 	cli
