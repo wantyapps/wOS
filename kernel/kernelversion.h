c6674813c07e64c6ca9c366c0cfa2694d3f67e25
Uri Arev <me@wantyapps.xyz>
2022-07-25 20:09:54 +0300

    Bump version

--- kernel/kernelversion.h
+++ kernel/kernelversion.h
@@ -2,7 +2,7 @@
 #define KERNELVERSION
 #define VERSION 1
 #define PATCHLEVEL 5
-#define SUBLEVEL 0
+#define SUBLEVEL 2
 #define EXTRAVERSION ""
 #define NAME "7inch"
 #define FULLVERSION "1.5.0 (7inch)"
